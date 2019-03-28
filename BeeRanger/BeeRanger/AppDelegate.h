af54ed720baae81d1a74553dd4de25d6fcbe901f
sugc <2528397406@qq.com>
2019-03-28 23:22:58 +0800

    工程创建

---
+++ BeeRanger/BeeRanger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeeRanger
+//
+//  Created by sugc on 2019/3/28.
+//  Copyright © 2019 sugc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeeRanger/BeeRanger/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  BeeRanger
+//
+//  Created by sugc on 2019/3/28.
+//  Copyright © 2019 sugc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+}
+
+@end
